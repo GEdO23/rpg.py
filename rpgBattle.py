4d79a93f3ec43a1571bbab221d0a8d8f3eed154f
GEdO23 <rm99632@fiap.com.br>
2023-03-11 11:37:43 -0300

    Sistema de turnos concertado

--- rpgBattle.py
+++ rpgBattle.py
@@ -1,5 +1,6 @@
 # Importar modulo random para turnos mais realistas e desafiadores
 import random
+import time
 
 # Criação das informações dos "Objetos", ou seja, Jogadores e Inimigos
 # Abaixo define o objeto padrão como o Jogador
@@ -20,25 +21,21 @@ object_inf = [f'{obj} Health Points :',
 # Definindo status e hp do jogador como global. Porque? Porque sim, tenha fé que funciona!
 
 global stats
-global p_hp
 
 # Definindo os status padrões do jogador em seu nivel 1
 
-p_atk = 5
-lvl = 1
+player_stats = {
+    'p_hp':100, 
+    'p_atk':5
+    }
+
+lvl = 1 
 exp = 2000
 
 # Sistema de level e experiencia, onde a cada mil experiencia que voce obtem, voce aumenta de nivel por um.
+
 if exp >= (1000 * (exp / 1000)):
     lvl += int(exp / 1000)
-
-# Declaração de status do jogador em lista, para obtenção dos dados mais facilmente.
-
-player_stats = [p_hp,
-                p_atk,
-                lvl,
-                exp]
-
 
 # Sistema de identificação do tipo de Inimigo
 # Dependendo do inimigo, o sistema da diferentes tipos de vida, ataque e experiencia que serão dadas ao jogador
@@ -48,8 +45,8 @@ class Enemy:
         self.enemy_type = enemy_type
         if enemy_type == 'Bat':
             self.obj = enemy_type
-            self.e_hp = 20
-            self.e_atk = 5
+            self.e_hp = 100
+            self.e_atk = 20
             self.given_experience = 15
 
         self.enemy_stats = [self.e_hp,
@@ -70,26 +67,35 @@ enemy = Enemy('Bat')
 def player_turn():
     hit_chance = random.randint(0, 1)
     if hit_chance == 1:
+        enemy.e_hp -= player_stats['p_atk']
         print(f'\nPlayer attacked {enemy.enemy_type}! ooooo')
-        enemy.e_hp -= p_atk
+    
     elif hit_chance == 0:
         print('\nPlayer missed! xxxxx')
+    
     print(enemy.object_inf[0], enemy.e_hp, 'hp')
 
 # Turno de um inimigo, com a chance de acerto de um ataque e sua consequencia (WIP)
 
 def enemy_turn():
-    global p_hp
     hit_chance = random.randint(0, 1)
     if hit_chance == 1:
+        player_stats['p_hp'] -= enemy.e_atk
         print(f'\n{enemy.enemy_type} attacked Player! ooooo')
-        p_hp -= enemy.e_atk
+    
     elif hit_chance == 0:
         print(f'\n{enemy.enemy_type} missed! xxxxx')
-    print(object_inf[0], player_stats[0], 'hp')
+    
+    print(object_inf[0], player_stats['p_hp'], 'hp')
 
 # Sistema de loop, onde os turnos irão se repetir até o jogador ou inimigo terem seu hp como zero, ou seja, até eles perderem a vida
-
-while enemy.e_hp > 0:
+while enemy.e_hp > 0 or player_stats['p_hp'] > 0:
+    if player_stats['p_hp'] <= 0:
+        print('\nGAME OVER\n')
+        break
     player_turn()
+    if enemy.e_hp <= 0:
+        print('\nYOU WIN\n')
+        break
     enemy_turn()
+         
