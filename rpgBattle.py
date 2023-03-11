1e40a8578cbe7c15b044d1aa78f613c96ecdb3fe
GEdO23 <rm99632@fiap.com.br>
2023-03-11 14:55:49 -0300

    Implementado sistema de batalha e turno

--- rpgBattle.py
+++ rpgBattle.py
@@ -1,6 +1,6 @@
 # Importar modulo random para turnos mais realistas e desafiadores
-import random
-import time
+from random import randint
+from time import sleep
 
 # Criação das informações dos "Objetos", ou seja, Jogadores e Inimigos
 # Abaixo define o objeto padrão como o Jogador
@@ -26,16 +26,17 @@ global stats
 
 player_stats = {
     'p_hp':100, 
-    'p_atk':5
+    'p_atk':5,
+    'lvl':1, 
+    'exp':0
     }
 
-lvl = 1 
-exp = 2000
 
 # Sistema de level e experiencia, onde a cada mil experiencia que voce obtem, voce aumenta de nivel por um.
 
-if exp >= (1000 * (exp / 1000)):
-    lvl += int(exp / 1000)
+def levelup():
+    if player_stats['exp']>= (1000 * (player_stats['exp'] / 1000)):
+        player_stats['lvl'] += int(player_stats['exp'] / 1000)
 
 # Sistema de identificação do tipo de Inimigo
 # Dependendo do inimigo, o sistema da diferentes tipos de vida, ataque e experiencia que serão dadas ao jogador
@@ -45,10 +46,15 @@ class Enemy:
         self.enemy_type = enemy_type
         if enemy_type == 'Bat':
             self.obj = enemy_type
-            self.e_hp = 100
-            self.e_atk = 20
-            self.given_experience = 15
-
+            self.e_hp = 0
+            self.e_atk = 0
+            self.given_experience = 0
+        elif enemy_type == 'Green Slime':
+            self.obj = enemy_type
+            self.base_e_hp = 9
+            self.e_atk = 8
+            self.given_experience = 160
+        self.e_hp = self.base_e_hp
         self.enemy_stats = [self.e_hp,
                             self.e_atk,
                             self.given_experience
@@ -59,26 +65,34 @@ class Enemy:
 
 
 # Declaração do tipo de inimigo
-enemy = Enemy('Bat')
-
+enemy = Enemy('Green Slime')
 
 # Turno do jogador, com a chance de acerto de um ataque e sua consequencia
 
 def player_turn():
-    hit_chance = random.randint(0, 1)
+    # CHANCE DE ACERTO
+    # Caso 0, o jogador erra o ataque
+    # Caso 1, o jogador acerta o ataque
+
+    hit_chance = randint(0, 1)
     if hit_chance == 1:
         enemy.e_hp -= player_stats['p_atk']
         print(f'\nPlayer attacked {enemy.enemy_type}! ooooo')
-    
+        
     elif hit_chance == 0:
         print('\nPlayer missed! xxxxx')
-    
+
+    print(object_inf[0], player_stats['p_hp'], 'hp')    
     print(enemy.object_inf[0], enemy.e_hp, 'hp')
 
-# Turno de um inimigo, com a chance de acerto de um ataque e sua consequencia (WIP)
+# Turno de um inimigo, com a chance de acerto de um ataque e sua consequencia
 
 def enemy_turn():
-    hit_chance = random.randint(0, 1)
+    # CHANCE DE ACERTO
+    # Caso 0, o inimigo erra o ataque
+    # Caso 1, o inimigo acerta o ataque
+
+    hit_chance = randint(0, 1)
     if hit_chance == 1:
         player_stats['p_hp'] -= enemy.e_atk
         print(f'\n{enemy.enemy_type} attacked Player! ooooo')
@@ -86,16 +100,29 @@ def enemy_turn():
     elif hit_chance == 0:
         print(f'\n{enemy.enemy_type} missed! xxxxx')
     
-    print(object_inf[0], player_stats['p_hp'], 'hp')
+    print(object_inf[0], player_stats['p_hp'], 'hp')    
+    print(enemy.object_inf[0], enemy.e_hp, 'hp')
+
+
+def restart_hp():
+    enemy.e_hp = enemy.base_e_hp
+
 
 # Sistema de loop, onde os turnos irão se repetir até o jogador ou inimigo terem seu hp como zero, ou seja, até eles perderem a vida
-while enemy.e_hp > 0 or player_stats['p_hp'] > 0:
-    if player_stats['p_hp'] <= 0:
-        print('\nGAME OVER\n')
-        break
-    player_turn()
-    if enemy.e_hp <= 0:
-        print('\nYOU WIN\n')
-        break
-    enemy_turn()
-         
+def rpg_battle():
+    while enemy.e_hp > 0 or player_stats['p_hp'] > 0:
+        if player_stats['p_hp'] <= 0:
+            print('\nGAME OVER\n')
+            break
+        player_turn()
+        if enemy.e_hp <= 0:
+            print('\nYOU WIN\n')
+            player_stats['exp'] += enemy.given_experience
+            levelup()
+            break
+        enemy_turn()
+    restart_hp()
+
+rpg_battle()
+rpg_battle()
+rpg_battle()    
