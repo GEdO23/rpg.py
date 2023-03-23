ec415f716c4886b9d0b15d5fa654745550ea8285
GEdO23 <rm99632@fiap.com.br>
2023-03-22 22:14:12 -0300

    Adcionado novos arquivos

---
+++ metro.py
@@ -0,0 +1,51 @@
+
+blu_l = [
+    "Tucuruvi",
+    "Parada Inglesa",
+    "Jardim São Paulo - Ayrton Senna",
+    "Santana",
+    "Carandiru",
+    "Portuguesa - Tietê",
+    "Armênia",
+    "Tiradentes",
+    "Luz",
+    "São Bento",
+    "Sé",
+    "Japão - Liberdade",
+    "São Joaquim",
+    "Vergueiro",
+    "Paraíso",
+    "Ana Rosa",
+    "Vila Mariana",
+    "Santa Cruz",
+    "Praça da Árvore",
+    "Saúde",
+    "São Judas",
+    "Conceição",
+    "Jabaquara"
+]
+
+i = 0
+l = 0
+
+
+def main():
+    print(f"Previous Station : {previous_station}")
+    print(f"Current Station : {current_station}")
+    print(f"Next Station : {next_station}")
+
+    submit = input("WHERE TO GO ? (next/back)\n").lower()
+    
+    if submit == "next":
+        current_station = next_station
+
+
+
+while True:
+ 
+
+    main()
+
+
+
+# Listra
